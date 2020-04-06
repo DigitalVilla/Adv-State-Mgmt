a0d4bed2d5519bafd9d682bf016f539f9252e48f
Omar Villanueva <o.villanueva@thearcane.com>
2020-04-05 20:50:37 -0600

    Container pattern: middle component just to manage state

--- 01_basics/src/Counter.js
+++ 01_basics/src/Counter.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
 
-export default class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    handleCalculation = (e) => {
-        const calc = e.target.getAttribute("data-id");
-        this.setState((prevState) => {
-            let counter = prevState.counter
-            switch (calc) {
-                case 'inc':
-                    counter += 1
-                    break;
-                case 'dec':
-                    counter -= 1
-                    break;
-                case 'res':
-                    counter = 0
-                    break;
-            }
-
-            return { counter }
-        })
-    }
-
-    render() {
-        return (
-            <section className="Counter">
-                <h1>Count: {this.state.counter}</h1>
-                <button onClick={this.handleCalculation} className="full-width" data-id="inc">Increment</button>
-                <button onClick={this.handleCalculation} className="full-width" data-id="dec">Decrement</button>
-                <button onClick={this.handleCalculation} className="full-width" data-id="res">Reset</button>
-            </section>
-        );
-    }
+export default function Counter(props) {
+    return (
+        <section className="Counter">
+            <h1>Count: {props.counter || 0}</h1>
+            <button onClick={props.onClick} className="full-width" data-id="inc">Increment</button>
+            <button onClick={props.onClick} className="full-width" data-id="dec">Decrement</button>
+            <button onClick={props.onClick} className="full-width" data-id="res">Reset</button>
+        </section>
+    );
 }
--- 01_basics/src/CounterContainer.js
+++ 01_basics/src/CounterContainer.js
@@ -1,1 +1,33 @@
 // Intentionally left blank.
+import React, { Component } from 'react';
+import Counter from './Counter'
+
+export default class CounterContainer extends Component {
+    state = {
+        counter: 0
+    }
+
+    handleCalculation = (e) => {
+        const calc = e.target.getAttribute("data-id");
+        this.setState((prevState) => {
+            let counter = prevState.counter
+            switch (calc) {
+                case 'inc':
+                    counter += 1
+                    break;
+                case 'dec':
+                    counter -= 1
+                    break;
+                case 'res':
+                    counter = 0
+                    break;
+            }
+
+            return { counter }
+        })
+    }
+
+    render() {
+        return ( <Counter onClick={this.handleCalculation} counter={this.state.counter} />);
+    }
+}
